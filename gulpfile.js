5482ebd3151e3d93fdc53e191cbb4bfb1cfa5281
Pshepu <paoloasepulveda@gmail.com>
2025-09-03 18:05:58 -0400

    commit del app.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 import { src, dest, watch } from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
-
+ 
 const sass = gulpSass(dartSass)
-
-export function css (done ){
-    src('src/scss/app.scss')
-        .pipe(sass().on('error',sass.logError) )
-        .pipe( dest('bluid/css'))
-
+ 
+export function css(done) {
+    src ('src/scss/app.scss')
+        .pipe( sass () )
+        .pipe( dest ('build/css'))    
+ 
     done()
 }
-
+ 
 export function dev() {
-    watch('src/scss/**/*.scss')
+    watch('src/scss/app.scss', css)
 }
